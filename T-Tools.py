f21439b98fcc92d1918fece92e24209c103211df
Tommaso Bona <piergeolo@gmail.com>
2022-08-22 23:20:30 +0200

    Update T-Tools.py

--- T-Tools.py
+++ T-Tools.py
@@ -4,19 +4,13 @@ import requests
 from pprint import pprint
 from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin
-import logging
+import colorama
 os.system("clear")
 os.system("toilet T-Tools")
 print("Coded By: ParzivalHack")
 print("Github: github.com/ParzivalHack")
 print("License: The source code of this tool is under the GPL v.3 License.")
 print("© 2022 Tommaso Bona")
-print(" ＞＞＞＞>>>＞ [Menu] ＜<<<＜＜＜＜ ")
-print("            ➡ [1] XSS Scanner ")
-print("            ➡ [2] Web Crawler ")
-print("            ➡ [3] Coming soon ")
-options = int(input("Select Option: "))
-if options == 1:
 def get_all_forms(url):
     soup = bs(requests.get(url).content, "html.parser")
     return soup.find_all("form")
@@ -65,47 +59,3 @@ def scan_xss(url):
 if __name__ == "__main__":
     url = str(input("Target URL: "))
     print(scan_xss(url))
-    if options == 2:
-        logging.basicConfig(
-    format='%(asctime)s %(levelname)s:%(message)s',
-    level=logging.INFO)
-
-class Crawler:
-
-    def __init__(self, urls=[]):
-        self.visited_urls = []
-        self.urls_to_visit = urls
-
-    def download_url(self, url):
-        return requests.get(url).text
-
-    def get_linked_urls(self, url, html):
-        soup = BeautifulSoup(html, 'html.parser')
-        for link in soup.find_all('a'):
-            path = link.get('href')
-            if path and path.startswith('/'):
-                path = urljoin(url, path)
-            yield path
-
-    def add_url_to_visit(self, url):
-        if url not in self.visited_urls and url not in self.urls_to_visit:
-            self.urls_to_visit.append(url)
-
-    def crawl(self, url):
-        html = self.download_url(url)
-        for url in self.get_linked_urls(url, html):
-            self.add_url_to_visit(url)
-
-    def run(self):
-        while self.urls_to_visit:
-            url = self.urls_to_visit.pop(0)
-            logging.info(f'Crawling: {url}')
-            try:
-                self.crawl(url)
-            except Exception:
-                logging.exception(f'Failed to crawl: {url}')
-            finally:
-                self.visited_urls.append(url)
-
-if __name__ == '__main__':
-    Crawler(urls=str(input("URL to crawl"))).run()
