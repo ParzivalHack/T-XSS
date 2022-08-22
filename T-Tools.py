f86083e2a7e293ffdd11516650df49f258b76085
Tommaso Bona <piergeolo@gmail.com>
2022-08-22 23:17:55 +0200

    Update T-Tools.py

--- T-Tools.py
+++ T-Tools.py
@@ -66,4 +66,46 @@ if __name__ == "__main__":
     url = str(input("Target URL: "))
     print(scan_xss(url))
     if options == 2:
-        
+        logging.basicConfig(
+    format='%(asctime)s %(levelname)s:%(message)s',
+    level=logging.INFO)
+
+class Crawler:
+
+    def __init__(self, urls=[]):
+        self.visited_urls = []
+        self.urls_to_visit = urls
+
+    def download_url(self, url):
+        return requests.get(url).text
+
+    def get_linked_urls(self, url, html):
+        soup = BeautifulSoup(html, 'html.parser')
+        for link in soup.find_all('a'):
+            path = link.get('href')
+            if path and path.startswith('/'):
+                path = urljoin(url, path)
+            yield path
+
+    def add_url_to_visit(self, url):
+        if url not in self.visited_urls and url not in self.urls_to_visit:
+            self.urls_to_visit.append(url)
+
+    def crawl(self, url):
+        html = self.download_url(url)
+        for url in self.get_linked_urls(url, html):
+            self.add_url_to_visit(url)
+
+    def run(self):
+        while self.urls_to_visit:
+            url = self.urls_to_visit.pop(0)
+            logging.info(f'Crawling: {url}')
+            try:
+                self.crawl(url)
+            except Exception:
+                logging.exception(f'Failed to crawl: {url}')
+            finally:
+                self.visited_urls.append(url)
+
+if __name__ == '__main__':
+    Crawler(urls=str(input("URL to crawl"))).run()
